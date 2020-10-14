b434b8b8cc6a2f59753b99be4cd4e53e3ba11f1b
fj <castelloiv@gmail.com>
2020-10-14 16:35:13 -0400

    WHY

--- app.js
+++ app.js
@@ -17,16 +17,6 @@ app.use(cookieParser());
 app.use(express.json());
 
 ///Hmmm
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", req.headers.origin);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  );
-  next();
-});
 
 const mongoDB = process.env.MONGO_URI;
 console.log("$$", mongoDB);
